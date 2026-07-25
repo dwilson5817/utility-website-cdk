bea61111cc481797f0154d92b6c5bb61746b093a
Dylan Wilson <dwilson5817@gmail.com>
2026-07-25 00:11:50 +0000

    feat(cdk): create Route53 hosted zone
    Create a hosted zone to manage DNS from within the CDK.

--- lib/utility-website-cdk-stack.ts
+++ lib/utility-website-cdk-stack.ts
@@ -1,10 +1,31 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
+import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class UtilityWebsiteCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const hostedZone = new route53.HostedZone(this, 'HostedZone', {
+      zoneName: 'tools.dylanw.net',
+    });
+
+    new route53.CaaRecord(this, 'AcmCaaRecord', {
+      zone: hostedZone,
+      values: [
+        { flag: 0, tag: route53.CaaTag.ISSUE, value: 'amazon.com' },
+        { flag: 0, tag: route53.CaaTag.ISSUE, value: 'amazontrust.com' },
+        { flag: 0, tag: route53.CaaTag.ISSUE, value: 'awstrust.com' },
+        { flag: 0, tag: route53.CaaTag.ISSUE, value: 'amazonaws.com' },
+        { flag: 0, tag: route53.CaaTag.ISSUE, value: 'letsencrypt.org' },
+      ],
+    });
+
+    new cdk.CfnOutput(this, 'HostedZoneNameServers', {
+      description: 'NS records to add to dylanw.net to delegate dmarc.dylanw.net to Route 53',
+      value: cdk.Fn.join(', ', hostedZone.hostedZoneNameServers!),
+    });
 
     const artifactsBucket = new s3.Bucket(this, 'ArtifactsBucket', {
       versioned: true,
